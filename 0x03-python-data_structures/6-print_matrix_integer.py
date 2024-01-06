5784dd6bc7b35f61a08d8f8bcae31e3ef4cf8aa0
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-01-06 17:40:17 +0100

    removed extra space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     """Prints the matrix of an integer"""
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
-            # if col != 0:
-            #     print(" ", end="")
-            print("{:d}".format(matrix[row][col]), end=" ")
+            if col != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[row][col]), end="")
         print()
