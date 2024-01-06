c3b324e22fb00e7d772d82dfbb22cf572c0f9936
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-01-06 17:34:55 +0100

    fixed syntax

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """Prints the matrix of an integer"""
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if col != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[row][col]), end=" ")
         print()
