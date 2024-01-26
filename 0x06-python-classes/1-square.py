47817f99e0f37949546bce8f12a2c1c8789957ee
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-01-27 00:27:41 +0100

    initialize a new square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
