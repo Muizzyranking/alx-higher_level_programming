9ebbfd982e1374d0822838f8419800f4308aa141
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-07 18:31:05 +0100

    fixed docs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""Module to add two integers"""
+"""Module for add_integer
+    can only add integers or floats
+    otherwise raise a TypeError
+    return the sum of a and b
+"""
 
 
 def add_integer(a, b=98):
-    """Function to add two integers"""
+    """adds two integers
+    @a: first integer
+    @b: second integer"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
