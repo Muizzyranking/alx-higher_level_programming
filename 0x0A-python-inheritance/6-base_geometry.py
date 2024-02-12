c2aa803cf7caafe98bcea5b15cafc897b6dd8cd2
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-12 02:50:36 +0100

    fixed typo error

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """Area of the geometry"""
-        raise Exception("area() is not imprmented")
+        raise Exception("area() is not implemented")
