f5b7566ad5fea4e39a00038c37417ea6bcd76dce
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-12 02:49:32 +0100

    base geometry with area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represensts a BaseGeometry"""
+
+    def area(self):
+        """Area of the geometry"""
+        raise Exception("area() is not imprmented")
