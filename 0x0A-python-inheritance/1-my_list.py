a1e122e23758e71fc2ce107f6806f7202a729392
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-12 02:10:20 +0100

    fixed main file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,10 +9,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints sorted list"""
-        sorted_list = sorted(self)
-        print(sorted_list)
-
-
-my_list_instance = MyList([3, 1, 4, 1, 5, 9, 2, 6, 5])
-sorted_list = my_list_instance.print_sorted()
-sorted_list
+        print(sorted(self))
