697026e2b2d36b025fed77ec5f71cd4a055e830e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-06 22:59:02 +0100

    readded

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,5 @@
 -- creates a table with defaults to 1 and unique
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT DEFAULT 1 UNIQUE,
+  name VARCHAR(256) NOT NULL
+);
