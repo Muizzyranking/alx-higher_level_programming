f637c1062a3b9f0ba8774cf5d87fc1c5eb7bb4cd
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-09 22:52:39 +0100

    add two arguments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,9 +2,9 @@
 // adds the first two arguments
 
 function add(a, b) {
-  return a + b;
+  return a + b
 }
 
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-console.log(add(a, b));
+const a = Number(process.argv[2])
+const b = Number(process.argv[3])
+console.log(add(a, b))
