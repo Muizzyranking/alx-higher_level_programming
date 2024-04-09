9d9e8a2fa7735b513d255b8d6af121b0fb2376b3
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-09 22:53:37 +0100

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // adds the first two arguments
 
-function add(a, b) {
-  return a + b
+function add (a, b) {
+  return a + b;
 }
 
-const a = Number(process.argv[2])
-const b = Number(process.argv[3])
-console.log(add(a, b))
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+console.log(add(a, b));
