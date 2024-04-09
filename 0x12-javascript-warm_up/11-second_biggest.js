84657d28b65a646844a8b16f19692cae8603f415
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-09 23:20:16 +0100

    gets the second largest value from arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const args = [];
+let biggest, secondBiggest;
+for (let i = 2; i < process.argv.length; i++) {
+  args.push(parseInt(process.argv[i]));
+}
+if (args.length < 2) {
+  console.log(0);
+} else {
+  let i = 0;
+  biggest = args[i];
+  secondBiggest = 0;
+  for (i = 1; i < args.length; i++) {
+    if (args[i] > biggest) {
+      secondBiggest = biggest;
+      biggest = args[i];
+    } else if (args[i] > secondBiggest) {
+      secondBiggest = args[i];
+    }
+  }
+}
+
+console.log(secondBiggest);
