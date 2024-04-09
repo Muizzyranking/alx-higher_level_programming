70aec24bd5eae0926c0cfa22b9f012165bfa54cd
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-09 18:20:35 +0100

    concatenates first two string

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// concatenates the first two arguments with "is" and print
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+console.log(firstArg + ' is ' + secondArg);
