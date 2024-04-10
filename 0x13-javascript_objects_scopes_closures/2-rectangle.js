49aef15bfc4176e8df57cdf72a1e10f42e8dec66
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-10 21:01:15 +0100

    checks if height and width are greater than 0

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// creates a class Rectangle that defines a rectangle
+// with width and height
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
