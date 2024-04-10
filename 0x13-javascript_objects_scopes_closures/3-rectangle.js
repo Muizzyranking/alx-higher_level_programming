0f97708b94709f7929bf63f437d71d68aa89f47c
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-10 21:08:23 +0100

    added instance method to print triangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// creates a class Rectangle that defines a rectangle
+// with width and height and instace method print()
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
