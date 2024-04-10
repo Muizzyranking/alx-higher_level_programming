6b5c07642509e01f2e5bf16012a88ea6a504f865
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-10 21:30:40 +0100

    charprint method to prints a custom character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// square that inherits from Rectangle
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  // prints a customn character
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
