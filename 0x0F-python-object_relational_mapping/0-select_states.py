98aebf33cce966edf59d86491df55b7c0611a4cf
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-14 10:18:41 +0100

    pycodestyle compliant

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa 
-    usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+""" Script that lists all states from the database hbtn_0e_0_usa
+    usage: ./0-select_states.py
+        <mysql username> <mysql password> <database name>
 """
 
 from sys import argv
