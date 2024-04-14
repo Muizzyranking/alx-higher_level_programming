af1281dc6d99fc786d05d7019f5376c3762b1657
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-14 14:03:13 +0100

    filter state by argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" List all states that start with 'N' """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    sql_user = argv[1]
+    sql_pass = argv[2]
+    sql_db = argv[3]
+    search_state = argv[4]
+
+    db = MySQLdb.connect(user=sql_user, passwd=sql_pass, db=sql_db)
+    cursor = db.cursor()
+    statement = """
+        SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC
+        """
+    cursor.execute(statement.format(search_state))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
