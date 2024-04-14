dc47cd02fda676e5e6c22965d8ed8e52f79f866f
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-14 13:33:15 +0100

    filter states by letter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" List all states that start with 'N' """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    sql_user = argv[1]
+    sql_pass = argv[2]
+    sql_db = argv[3]
+
+    db = MySQLdb.connect(user=sql_user, passwd=sql_pass, db=sql_db)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
