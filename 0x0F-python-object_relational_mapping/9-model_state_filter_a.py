87ef660f88ed9b54a64b4764a5276ded59f8e857
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-15 16:59:57 +0100

    select values with filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "main":
+    sql_user = argv[1]
+    sql_pass = argv[2]
+    sql_db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sql_user, sql_pass, sql_db),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
+    for state in result:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
