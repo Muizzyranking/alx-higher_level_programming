0666e961df42eb8488ed999f6e228163212ea436
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-16 02:59:28 +0100

    prints cities and their states

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all Cities objects, and corresponding States objects, contained in the"""
+
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    sql_user = argv[1]
+    sql_pass = argv[2]
+    sql_db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sql_user, sql_pass, sql_db),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city in session.query(City).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
