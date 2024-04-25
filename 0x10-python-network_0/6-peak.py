d41aef4c6b83310b45a6dc082c318f8e6f54664b
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-25 18:59:26 +0100

    peak g

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,20 +3,22 @@
 
 
 def find_peak(list_of_integers):
-    """find peak"""
+    """Finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
+
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        return max(list_of_integers)
 
     mid = len(list_of_integers) // 2
-    peak = list_of_integers[mid]
 
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+    if list_of_integers[mid] >= list_of_integers[mid - 1] \
+            and list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+
+    # Check if peak is in the left half
+    if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
         return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid:])
+
+    # Peak must be in the right half
+    return find_peak(list_of_integers[mid + 1:])
