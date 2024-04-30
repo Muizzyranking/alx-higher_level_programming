36ef9af4071c36c1b4a3206b3c978a56a63240cc
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-30 19:08:37 +0100

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-from urllib import request
+"""a Python script that fetches `https://alx-intranet.hbtn.io/status`"""
+import urllib.request
+
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("utf-8")))
