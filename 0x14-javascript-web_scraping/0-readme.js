380df42aa550a729facf69b332e256d15bb3a54a
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-23 12:00:11 +0100

    read and log file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// reads the content of a file and log it o the console
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+        console.log(err);
+    return;
+  }
+  console.log(data);
+});
