933e0a4e2456eedc0c83678338cb9a25fed17823
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-24 15:07:25 +0100

    get titile with id

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// returns title of a movie by its id
+
+const request = require('request');
+
+function getTitle (id) {
+    const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+    request(url, (error, response, body) => {
+        if (error) {
+        console.log(error);
+        } else {
+        console.log(JSON.parse(body).title);
+        }
+    });
+}
+
+if (process.argv.length === 3) {
+    if (isNaN(process.argv[2])) {
+        console.log('Usage: ./3-starwars_title.js <movie_id>');
+        console.log('movie_id must be an integer');
+        return;
+    }
+    getTitle(process.argv[2]);
+} else {
+    console.log('Usage: ./3-starwars_title.js <movie_id>');
+}
