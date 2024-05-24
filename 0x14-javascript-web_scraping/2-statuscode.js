8e57ca8eb1841a3e03e9bd8a33ec081f26ef5187
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-24 14:41:15 +0100

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,10 @@
 const url = process.argv[2];
 const request = require('request');
 
-request(url, function (error, response){
-    if (error) {
-        console.log(error);
-        return;
-    }
-    console.log('code:', response.statusCode);
+request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('code:', response.statusCode);
 });
