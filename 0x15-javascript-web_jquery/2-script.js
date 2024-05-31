0d200f3b0d606d90b607c3c9dd1b4462af9d71c5
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-06-01 00:24:53 +0100

    (feat): script to modify element css on click

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+console.log('Script loaded');
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.getElementById('red_header')
+    header.addEventListener("click", function() {
+        header.style.color = '#FF0000';
+    })
+});
+console.log('Script loaded');
